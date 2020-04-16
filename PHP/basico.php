b2c6166b27366150c5561e025db103352e934ad9
Luiz Felipe <campos.luiz97@gmail.com>
2020-04-16 17:13:41 -0300

    Primeiros comandos

--- PHP/basico.php
+++ PHP/basico.php
@@ -36,7 +36,7 @@
     */
     echo "<strong> 2 - VARIAVEIS PRÉ DEFINIDAS (SUPER GLOBAIS) </strong> <br/>";  
 
-    $URL = (float)$_GET["a"]; /// CAST
+    //$URL = (float)$_GET["a"]; /// CAST & pegar valor por URL
     $IP = $_SERVER["REMOTE_ADDR"]; /// PEGAR O IP 
     $nomeArquivo = $_SERVER["SCRIPT_NAME"]; /// NOME DO ARQUIVO PHP EXECUTADO.
 
@@ -71,6 +71,9 @@
          === --> COMPARA OS VALORES E OS TIPOS (IGUALDADE DE IDENTIDADE)
          != -> COMPARA VALOR
          !== -> COMPARA VALOR E TIPO DE DADO
+         <=> -> space chip -1 caso o da DIREITA é maior 0 caso sejam IGUAIS e 1 caso o da ESQUERDA é maior
+         ?? -> coalesce
+         $a++ ou ++$a -> 
     */       
     echo "<br/> <strong> 4 - OPERADORES </strong> <br/>"; 
 
@@ -102,8 +105,54 @@
     $a = 10;
     $b = 35;
 
-    var_dump($a !== $b);
+    echo 'space chip' . var_dump($a <=> $b) . '<br />';
 
+    $a = NULL;
+    $b = 20;
 
+     echo $a ?? $b . '<br />'; // se a variavel A for NULA, exibir B.. assim por diante 
+
+    // ******************* String *********************
+    /* 
+        Interpolação de variaveis (utilizar aspas simple)
+        strtoupper -> deixar o valor em UPPER CASE
+        strtolower -> DEIXAR EM lower
+        ucwords -> Deixar a primeira letra MAIUSCULA de cada palavra.
+        ucfirst -> Deixa a primeira letra da primeira palavra em MAIUSCULA
+        str_replace -> Alterar letras na string.
+        strpos -> indice da palavra que quero buscar
+        substr -> pegar palavra entre indices
+        strlen -> tamanho da string;
+    */       
+    echo "<br/> <strong> 5 - STRINGS </strong> <br/>"; 
+
+    $aspasDupla = "Teste aspas duplas";
+    $numero = 30;
+    $aspasSimples = 'Teste aspas simples';
+
+    echo "INTERPOLAÇÃO DE VARIAVEIS ->  $aspasDupla MAIS outra variavel SEM concatenar $numero <br /><br />" ;
+
+    $testeUpper = "teste upper case";
+
+    echo "upper case de variavel ->".strtoupper($testeUpper)."<br />"; 
+
+    $nome = "luiz felipe campos <br />";
+
+    echo ucwords($nome); 
+
+    $testeReplace = "Teste replace";
+
+    echo str_replace("e","3", $testeReplace)."<- REPLACE <br />";
+
+    $testePos = "Maria santos aparecida";
+    echo strpos($testePos, "santos")." <- POSIÇÃO <br />";
+
+    echo substr($testePos, 0, strlen($testePos)-3)." <- SUB STRING <br />";
+
+    // ******************* INCLUDE && REQUIRE *********************
+    /* 
+        
+    */       
+    echo "<br/> <strong> 6 - INCLUDE && REQUIRE </strong> <br/>"; 
 
 ?>
