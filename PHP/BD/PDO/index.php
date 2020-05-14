5e94b6b5e2ae4bf94274842bfd02e16490f39ad6
Luiz Felipe <campos.luiz97@gmail.com>
2020-05-14 17:06:00 -0300

    PDO + DAO

--- PHP/BD/PDO/index.php
+++ PHP/BD/PDO/index.php
@@ -11,9 +11,9 @@
     echo "<strong> 1 - CONEXÃO BD COM PDO </strong> <br/>";
 
     
-    //$conn = new PDO("mysql:dbname=curso;host=localhost","root",""); // MYSQL
+    $conn = new PDO("mysql:dbname=curso;host=localhost","root",""); // MYSQL
     
-    $conn = new PDO("sqlsrv:Database=curso;server=.;ConnectionPooling=0","sa","123"); // SQL SERVER
+    //$conn = new PDO("sqlsrv:Database=curso;server=.;ConnectionPooling=0","sa","123"); // SQL SERVER
 
     echo "Conectado com sucesso! <br>";
 
@@ -34,6 +34,37 @@
 
         echo "=============================================== <br/>";
     }
+
+
+    echo "<br/> <strong> 3 - INCLUIR REGISTRO </strong> <br/>";     
+
+
+    $conn = new PDO("mysql:dbname=curso;host=localhost","root","");
+
+    $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN,:SENHA)");
+
+    $login = "maria";
+    $senha = "99999996441";
+
+    $stmt->bindParam(":LOGIN",$login);
+    $stmt->bindParam(":SENHA",$senha);
+
+   // $stmt->execute();
+
+    echo "Incluiu o login: <strong>" . $login . "</strong> com sucesso!!";
+
+    echo "<br/> <strong> 4 - TRANSAÇÕES </strong> <br/>";
+
+    $conn->beginTransaction(); // INICIANDO A TRANSAÇÃO
+
+    $stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = ?");
+    
+    $stmt->execute(array(3)); //NOVA MANEIRA DE PASSAR PARÂMETRO 
+
+    $conn->rollBack();
+    //$conn->commit();
+
+    echo "Delete OK";
 
     
 
