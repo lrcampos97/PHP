8abda24cfcad1eb6ef5f771316e7135770af85ad
Luiz Felipe Campos <campos.luiz97@gmail.com>
2020-07-06 07:05:24 -0300

    Composer

---
+++ PHP/Composer/RainTPL/cache/index.da73fd04b31c152ffe8c68bb14d64d89.rtpl.php
@@ -0,0 +1,4 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h2> Teste de Temple do <strong><?php echo htmlspecialchars( $projeto, ENT_COMPAT, 'UTF-8', FALSE ); ?></strong> </h1>
+
+<p> Utilizando a biblioteca RainTPL para templates</p>
+<p> Version: <strong><?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?></strong></p>
---
+++ PHP/Composer/index.php
@@ -0,0 +1,15 @@
+<?php
+    /*  
+        COMPOSER 
+
+    -> composer init : criar um novo arquivo composer, com as dependencias
+    -> composer update : vai fazer o download de todas as dependencias presentes no composer.json
+    -> arquivo composer.lock : tem as informações do download dos repositório, dos repositórios que ele configurou
+    -> pasta vendor : são os vendor das dependências, por exemplo o monolog também pode ter alguma dependência  
+    -> arquivo vendor/autoload.php : ele carrega automaticamente a classe quando eu faço o NILL e o name space da classe
+
+    */
+
+    echo "<br/> <strong> 1 - COMPOSER (configurações)  </strong> <br/>"; 
+
+?>
