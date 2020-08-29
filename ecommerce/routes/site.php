5bf2dfe75618b658f5754a8332410ffd28a2c32b
lrcampos97 <campos.luiz97@gmail.com>
2020-08-29 11:03:02 -0300

    feat(AlteraSenha): Nova funcionalidade para alterar senha
    Adicionado novas rotas para o usuário alterar a senha.

--- ecommerce/routes/site.php
+++ ecommerce/routes/site.php
@@ -557,6 +557,77 @@ $app->get('/profile/orders/:idorder', function($idorder){
 });
 
 
+// ALTERAR SENHA 
+$app->get('/profile/change-password', function(){
+	
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl('profile-change-password',[
+		"changePassError"=>User::getMsgError(),
+		"changePassSuccess"=>User::getSuccess()
+	]);
+});
+
+// ALTERAR SENHA POST 
+$app->post('/profile/change-password', function(){
+	
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+
+		User::setMsgError("Digite a senha atual.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+
+		User::setMsgError("Digite a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+
+		User::setMsgError("Confirme a nova senha.");
+		header("Location: /profile/change-password");
+		exit;
+
+	}	
+
+	if ($_POST['current_pass'] === $_POST['new_pass']) {
+
+		User::setMsgError("A sua nova senha deve ser diferente da atual.");
+		header("Location: /profile/change-password");
+		exit;		
+
+	}	
+
+	$user = User::getFromSession();
+
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setMsgError("A senha está inválida.");
+		header("Location: /profile/change-password");
+		exit;			
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+	
+	User::setSuccess("Senha alterada com sucesso.");
+
+	header("Location: /profile/change-password");
+	exit;
+});
+
+
 // BOLETO
 $app->get('/boleto/:idorder', function($idorder){
 
