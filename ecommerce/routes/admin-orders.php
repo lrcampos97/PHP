6dab9b9b4073e6bd8e950a807df9e446815cc2e3
lrcampos97 <campos.luiz97@gmail.com>
2020-08-31 10:57:34 -0300

    feat(Pedidos): Paginação e busca de PEDIDOS admin
    Novo recurso no painel administrativo para buscar e paginar os PEDIDOS.

--- ecommerce/routes/admin-orders.php
+++ ecommerce/routes/admin-orders.php
@@ -64,16 +64,45 @@ $app->get("/admin/orders/:idorder/delete", function($idorder){
 });
 
 
-// INDEX DO E-COMMERCE
 $app->get('/admin/orders', function() {
 
 	User::verifyLogin();
 
-	$page = new Ecommerce\PageAdmin();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-	$page->setTpl("orders",[
-        "orders"=>Order::listAll()
-    ]);
+	if ($search != '') {
+
+		$pagination = Order::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Order::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/orders?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
+
+	$page = new PageAdmin();
+
+	$page->setTpl("orders", [
+		"orders"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);
 	
 });
 
