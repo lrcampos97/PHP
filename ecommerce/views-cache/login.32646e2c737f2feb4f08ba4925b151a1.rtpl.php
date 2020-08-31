19c4232f69a6997a29406cf5f7a29e25c4a9c8fa
lrcampos97 <campos.luiz97@gmail.com>
2020-08-31 11:29:41 -0300

    feat(Usuários): Alterar senha usuário admin
    Adicionado funcionalidade para alterar a senha de usuários administradores

--- ecommerce/views-cache/login.32646e2c737f2feb4f08ba4925b151a1.rtpl.php
+++ ecommerce/views-cache/login.32646e2c737f2feb4f08ba4925b151a1.rtpl.php
@@ -58,13 +58,13 @@
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
+    <!-- <div class="social-auth-links text-center">
       <p>- OR -</p>
       <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
         Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
         Google+</a>
-    </div>
+    </div> -->
     <!-- /.social-auth-links -->
 
     <a href="/admin/forgot">I forgot my password</a><br>
